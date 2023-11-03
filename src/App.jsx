6c1fe36dedf728541ecbc2927335add25144584a
Anas Ansari <121580678+annasansari@users.noreply.github.com>
2023-11-04 01:37:06 +0500

    some changes in Client side

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import React, { useState } from 'react'
 import SignIn from './pages/SignIn'
 import Home from './pages/Home'
 import About from './pages/About'
@@ -7,11 +8,11 @@ import SignUp from './pages/SignUp'
 import Header from './components/Header'
 
 function App() {
-
+// const [searchBar, setSearchBar] = useState(false)
   return (
     <>
       <BrowserRouter>
-        <Header />
+        <Header searchBar={false}/>
         <Routes>
           <Route path='/' element={<Home />} />
           <Route ute path='/about' element={<About />} />
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 function SignUp() {
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Sign Up</h1>
-      <form className='flex flex-col gap-3' action='#'>
-        <input className='border p-3 rounded-lg active:outline-none' id='username' type="text" placeholder='Username' />
-        <input className='border p-3 rounded-lg active:outline-none' id='email' type="email" placeholder='Email' />
-        <input className='border p-3 rounded-lg active:outline-none' id='password' type="password" placeholder='Password' />
-        <button className='bg-slate-800 border rounded-md p-2 text-white text-lg font-medium hover:opacity-80 uppercase' >Sign Up</button>
+      <form className='flex flex-col gap-3'>
+        <input className='border p-3 rounded-lg active:outline-none' onChange={handleChange} id='username' type="text" placeholder='Username' />
+        <input className='border p-3 rounded-lg active:outline-none' onChange={handleChange} id='email' type="email" placeholder='Email' />
+        <input className='border p-3 rounded-lg active:outline-none' onChange={handleChange} id='password' type="password" placeholder='Password' />
+        <button onClick={HandleSubmit} className='bg-slate-800 border rounded-md p-2 text-white text-lg font-medium hover:opacity-80 uppercase' >Sign Up</button>
       </form>
       <div className='flex gap-2 mt-5'>
         <p>Have an account?</p>
